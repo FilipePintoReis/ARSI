a7fdcf7acabec705ae036bb3de8b72335fa72cac
Filipe Pinto Reis <filipepintodosreis@gmail.com>
2020-03-31 00:56:11 +0100

    clustering coefficient, bridges and average embeddedness

--- src/snap/main.py
+++ src/snap/main.py
@@ -30,9 +30,66 @@ def populate(network):
             else: 
                 c += 1
 
+def showClusteringCoefficient(network):
+    DegToCCfV = snap.TFltPrV()
+    result = snap.GetClustCfAll(network, DegToCCfV)
+    for item in DegToCCfV:
+        print("degree: %d, clustering coefficient: %f" % (item.GetVal1(), item.GetVal2()))
+    print("average clustering coefficient", result[0])
+    print("closed triads", result[1])
+    print("open triads", result[2])
+
+
+def getEdgeBridges(network):
+    UGraph = snap.ConvertGraph(snap.PUNGraph, network)
+
+    EdgeV = snap.TIntPrV()
+    snap.GetEdgeBridges(UGraph, EdgeV)
+
+    for edge in EdgeV:
+        print("edge: (%d, %d)" % (edge.GetVal1(), edge.GetVal2()))
+    print(len(EdgeV))
+    return EdgeV
+
+def getAverageEmbeddednessOfEdges(network):
+    m = {}
+    for edge in network.Edges():
+        if edge.GetSrcNId() in m:
+            m[edge.GetSrcNId()].append(edge.GetDstNId())
+        else:
+            m[edge.GetSrcNId()] = [edge.GetDstNId()]
+     
+    counter = 0
+    for edge in network.Edges():
+        for n1 in m[edge.GetSrcNId()]:
+            if edge.GetDstNId() in m and n1 in m[edge.GetDstNId()]:
+                counter += 1
+
+    return counter/network.GetEdges()
+
+def getAnEdgeEmbeddedness(network, edge):
+    m = {}
+    for e in network.Edges():
+        if e.GetSrcNId() in m:
+            m[e.GetSrcNId()].append(e.GetDstNId())
+        else:
+            m[e.GetSrcNId()] = [e.GetDstNId()]
+    
+    counter = 0
+    for n1 in m[edge.GetSrcNId()]:
+        if edge.GetDstNId() in m and n1 in m[edge.GetDstNId()]:
+            counter += 1
+
+    return counter
+
 
 network = TNEANet.New()
 
 populate(network)
 
-snap.PlotClustCf(network, "example", "Directed graph - clustering coefficient")
+
+print(getAverageEmbeddednessOfEdges(network))
+
+#snap.PrintInfo(network, "Python type PNEANet")
+
+
